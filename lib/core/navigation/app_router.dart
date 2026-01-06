00e4e0155bf1c7dca37be039ebf29335a43e86f8
Koochoox <vickeythebackstreetguy@gmail.com>
2026-01-06 08:11:45 +0530

    Add flash route to app router

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -5,6 +5,7 @@ import '../../modules/chat/chat.dart';
 import '../../modules/freepay/freepay.dart';
 import '../../modules/moment/moment.dart';
 import '../../modules/summit/summit.dart';
+import '../../modules/flash/flash.dart';
 ///
 /// AppRouter
 /// Central navigation spine of Freeguy LIFE_OS
@@ -15,6 +16,7 @@ class AppRouter {
   static const String freepay = '/freepay';
   static const String moment = '/moment';
   static const String summit = '/summit';
+  static const String flash = '/flash';
 
   static Route<dynamic> generate(RouteSettings settings) {
     switch (settings.name) {
@@ -31,6 +33,9 @@ class AppRouter {
 
       case moment:
         return _page(const Moment());
+
+      case flash:
+        return _page(const Flash());
 
       default:
         return _page(const Dashboard());
