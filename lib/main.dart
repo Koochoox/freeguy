281f41cb1025fd0f1d76b35a3c2bca05c41469cf
Koochoox <vickeythebackstreetguy@gmail.com>
2026-01-06 08:41:34 +0530

    Refactor imports and update FreeguyApp settings boot: wire main app to router
    Updated import paths and modified the FreeguyApp configuration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'core/navigation/app_router.dart';
-import 'core/system/launch_freeze.dart';
+import 'core/router/app_router.dart';
 
 void main() {
-  LaunchFreeze.lock();
   runApp(const FreeguyApp());
 }
+
 class FreeguyApp extends StatelessWidget {
   const FreeguyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Freeguy',
       debugShowCheckedModeBanner: false,
-      onGenerateRoute: AppRouter.generate,
       initialRoute: AppRouter.dashboard,
+      onGenerateRoute: AppRouter.generate,
     );
   }
 }
