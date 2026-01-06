6e2b5911f70e6fc1ef640ba4a9935590b2d0e229
Koochoox <vickeythebackstreetguy@gmail.com>
2026-01-06 07:03:34 +0530

    Add import for chat module in app_router.dart

--- lib/core/navigation/app_router.dart
+++ lib/core/navigation/app_router.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../../modules/dashboard/dashboard.dart';
 
+import '../../modules/chat/chat.dart';
+
 ///
 /// AppRouter
 /// Central navigation spine of Freeguy LIFE_OS
